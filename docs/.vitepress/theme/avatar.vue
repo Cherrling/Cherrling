c7313e905f7aebe3f2c983e20081f2c4acfdaca0
白日梦主义 <37233261+Cherrling@users.noreply.github.com>
2025-02-15 22:18:58 +0800

    Update avatar.vue

--- docs/.vitepress/theme/avatar.vue
+++ docs/.vitepress/theme/avatar.vue
@@ -9,7 +9,7 @@ const { Layout } = DefaultTheme
     <template #sidebar-nav-before>
         <a href="/">
             <!-- <img style='border-radius: 50%;margin: 10px;' src="https://docsify-archive.cherrling.pages.dev/docs/assets/README/6FE475F45003FA1E7961C5F6F1736203D7C120CD982A1A08A730B155D5336F0D.jpg" alt=""> -->
-            <img style='border-radius: 50%;margin: 10px;'  src="https://img.923329.xyz/api/cfile/AgACAgQAAyEGAASRzsK6AAIBJGcqbY6Yqn34Zg63rlZBHAlwTpG1AAJRxTEbMilYUUENPyqPRrjEAQADAgADbQADNgQ">
+            <img style='border-radius: 50%;margin: 10px;'  src="https://iili.io/2pVlUKP.md.jpg">
             <div style="text-align: center;font-size: larger;"><span>Cherrling</span></div>
         </a>
     </template>
